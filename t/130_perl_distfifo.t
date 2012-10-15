5756692d14e612602b36304d15b36dd43bbc074a
Steffen Mueller <smueller@cpan.org>
2012-10-15 15:53:42 +0200

    DistQueue tests (Perl backends: naive and claim types)

--- t/130_perl_distfifo.t
+++ t/130_perl_distfifo.t
@@ -8,18 +8,35 @@ use Queue::Q::Test;
 use Queue::Q::TestDistFIFO;
 
 use Queue::Q::NaiveFIFO::Perl;
+use Queue::Q::ClaimFIFO::Perl;
 use Queue::Q::DistFIFO;
 
-my @q = map Queue::Q::NaiveFIFO::Perl->new(), 1..5;
+SCOPE: {
+    my @q = map Queue::Q::NaiveFIFO::Perl->new(), 1..5;
 
-isa_ok($_, "Queue::Q::NaiveFIFO") for @q;
-isa_ok($_, "Queue::Q::NaiveFIFO::Perl") for @q;
+    isa_ok($_, "Queue::Q::NaiveFIFO") for @q;
+    isa_ok($_, "Queue::Q::NaiveFIFO::Perl") for @q;
 
-my $q = Queue::Q::DistFIFO->new(
-    shards => \@q,
-);
-isa_ok($q, "Queue::Q::DistFIFO");
+    my $q = Queue::Q::DistFIFO->new(
+        shards => \@q,
+    );
+    isa_ok($q, "Queue::Q::DistFIFO");
 
-Queue::Q::TestDistFIFO::test_dist_fifo($q);
+    Queue::Q::TestDistFIFO::test_dist_fifo($q);
+}
+
+SCOPE: {
+    my @q = map Queue::Q::ClaimFIFO::Perl->new(), 1..5;
+
+    isa_ok($_, "Queue::Q::ClaimFIFO") for @q;
+    isa_ok($_, "Queue::Q::ClaimFIFO::Perl") for @q;
+
+    my $q = Queue::Q::DistFIFO->new(
+        shards => \@q,
+    );
+    isa_ok($q, "Queue::Q::DistFIFO");
+
+    Queue::Q::TestDistFIFO::test_dist_fifo_claim($q);
+}
 
 done_testing();
--- t/lib/Queue/Q/TestDistFIFO.pm
+++ t/lib/Queue/Q/TestDistFIFO.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Test::More;
 
+use Queue::Q::ClaimFIFO::Item qw(make_item);
+
 sub test_dist_fifo {
     my $q = shift;
 
@@ -62,6 +64,89 @@ sub test_dist_fifo {
             fail("Strict ordering checks: $_");
         }
     }
+
+    $q->enqueue_items(1..100);
+    is($q->queue_length, 100, "Queue len check 8");
+    $q->flush_queue;
+    is($q->queue_length, 0, "Queue len check 8");
+}
+
+sub qlen_claimcount {
+    my ($q, $qlen, $claimcount, $name) = @_;
+    $name = defined($name) ? "($name)" : "";
+    is($q->queue_length, $qlen, "Queue length is $qlen $name");
+    is($q->claimed_count, $claimcount, "Claimed count is $claimcount $name");
+}
+
+sub test_dist_fifo_claim {
+    my $q = shift;
+
+    # clean up so the tests make sense
+    $q->flush_queue;
+    qlen_claimcount($q, 0, 0, "Flushed queue has no items");
+
+    $q->enqueue_item(make_item($_)) for 1..2;
+    qlen_claimcount($q, 2, 0, "1");
+
+    my @items;
+    push @items, $q->claim_item;
+    qlen_claimcount($q, 1, 1, "2");
+
+    push @items, $q->claim_item();
+    qlen_claimcount($q, 0, 2, "3");
+
+    $q->enqueue_items(make_item($_)) for 151..161;
+    qlen_claimcount($q, 11, 2, "4");
+
+    @items = sort {$a->item_data <=> $b->item_data} @items; 
+    is(scalar(@items), 2);
+    is($items[0]->item_data, 1, "Fetched one item [1]");
+    is($items[1]->item_data, 2, "Fetched one item [2]");
+    $q->mark_item_as_done($_) for @items;
+    qlen_claimcount($q, 11, 0, "5");
+
+    @items = $q->claim_items();
+    qlen_claimcount($q, 10, 1, "6");
+
+    push @items, $q->claim_items(3);
+    qlen_claimcount($q, 7, 4, "7");
+    push @items, grep defined, $q->claim_items(15);
+    qlen_claimcount($q, 0, 11, "8");
+
+    @items = sort {$a->item_data <=> $b->item_data} @items;
+    is_deeply([map $_->item_data, @items], [151..161]);
+
+    $q->mark_items_as_done(@items);
+    qlen_claimcount($q, 0, 0, "9");
+
+    my @set1 = map make_item($_), (1..10);
+    my @set2 = map make_item($_), (11..20);
+    my @set3 = map make_item($_), (21..30);
+
+    $q->enqueue_items_strict_ordering(@set1);
+    $q->enqueue_items_strict_ordering(@set2);
+    $q->enqueue_items_strict_ordering(@set3);
+
+    for (1..30) {
+        my $item = $q->claim_item();
+        for my $s (\@set1, \@set2, \@set3) {
+            if (@$s and $s->[0]->item_data == $item->item_data) {
+                pass("Strict ordering checks: $_");
+                $q->mark_item_as_done($item);
+                $item = undef;
+                shift @$s;
+                last;
+            }
+        }
+        if (defined $item) {
+            fail("Strict ordering checks: $_");
+        }
+    }
+
+    $q->enqueue_items(map make_item($_), 1..100);
+    is($q->queue_length, 100, "Queue len check 8");
+    $q->flush_queue;
+    is($q->queue_length, 0, "Queue len check 8");
 }
 
 
