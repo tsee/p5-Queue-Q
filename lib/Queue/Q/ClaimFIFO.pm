b39f8517f4b3af27fdf2e181bba5e1eead31a312
Steffen Mueller <smueller@cpan.org>
2012-10-16 21:09:35 +0200

    More ClaimFIFO documentation

--- lib/Queue/Q/ClaimFIFO.pm
+++ lib/Queue/Q/ClaimFIFO.pm
@@ -40,6 +40,20 @@ Queue::Q::ClaimFIFO - FIFO queue keeping track of claimed items
 =head1 SYNOPSIS
 
   use Queue::Q::ClaimFIFO::Redis; # or ::Perl or ...
+  my $q = ... create object of chosen ClaimFIFO implementation...
+  
+  # producer:
+  $q->enqueue_item([qw(my data structure)]); # rinse repeat...
+  
+  # consumer:
+  my $item = $q->claim_item;
+  my $data = $item->item_data;
+  # work with data...
+  $q->mark_item_as_done($item);
+  
+  # Implementation dependent: somewhere in a recovery cron job
+  # - Fetch claimed items older than $n minutes/seconds
+  # - Requeue or log&drop those timed-out items
 
 =head1 DESCRIPTION
 
@@ -48,7 +62,33 @@ of all in-flight ("claimed") items. Implementations are
 required to provide strict ordering and adhere to
 the run-time complexities listed below (or better).
 
-All ...
+The general workflow with C<Queue::Q::ClaimFIFO> based
+queue is:
+
+=over 2
+
+=item *
+
+Producer enqueues one or multiple items.
+
+=item *
+
+Consumer claims one or multiple items and works on them.
+
+=item *
+
+Consumer marks its claimed items as done.
+
+=back
+
+To recover from failed consumers, one may apply in any one of many
+application specific recovery strategies such as periodically
+re-enqueuing all claimed items that are older than a threshold
+or possible simply clearing them out and logging the fact.
+
+Since the actual recovery strategy is application-dependent and the
+support by the queue implementation may vary, there's no API for this
+in this abstract base class. That may change in a future release.
 
 =head1 METHODS
 
@@ -58,6 +98,13 @@ Given a data structure, that data structure is added to
 the queue. Items enqueued with C<enqueue_item> in order
 must be returned by C<claim_item> in the same order.
 
+The data structure passed to C<enqueue_item> will be
+automatically wrapped in a L<Queue::Q::ClaimFIFO::Item>
+object by C<enqueue_item>. You cannot pass an object
+of that class (or its subclasses) as the (top-level)
+data structure to prevent using the same Item objects
+in multiple queues accidentally.
+
 Complexity: O(1)
 
 =head2 enqueue_items
@@ -66,6 +113,9 @@ Given a number data structures, enqueues them in order.
 This is conceptually the same as calling C<enqueue_item> multiple
 times, but may help save on network round-trips.
 
+As with C<enqueue_item> this will refused to accept
+prefabricated C<Queue::Q::ClaimFIFO::Item>s.
+
 Complexity: O(n) where n is the number of items to enqueue.
 
 =head2 claim_item
@@ -73,7 +123,24 @@ Complexity: O(n) where n is the number of items to enqueue.
 Returns the oldest item in the queue. Returns C<undef>
 if there is none left.
 
+This does not return the originally enqueued data structure
+directly but the C<Queue::Q::ClaimFIFO::Item> object that
+wraps it. It is this returned object that you need to pass to
+C<mark_item_as_done> to remove it from the tracked in-flight
+items.
+
 Complexity: O(1)
+
+Implementations may (but should not)
+deviate from the strict O(1) complexity
+for the number of B<claimed> items at any given time. That is
+acceptable as high as O(log(n)). This is likely acceptable
+because the number of items being worked on is likely not
+to be extremely large.
+Implementations that make use of this relaxed requirement
+must document that clearly.
+The number of B<queued> items
+must not affect the complexity, however.
 
 =head2 claim_items
 
@@ -87,6 +154,28 @@ If there are less than the desired number of items to be claimed,
 returns a correspondingly shorter list.
 
 Complexity: O(n) where n is the number of items claimed.
+
+See the documentation of C<claim_item> for a potential relaxation
+on the complexity bounds with respect to the number of
+in-flight, claimed items.
+
+=head2 mark_item_as_done
+
+Given a C<Queue::Q::ClaimFIFO::Item> object that was previously
+claimed from this queue, it is removed from the claimed-items
+tracking and thus removed from the queue altogether.
+
+Complexity: Generally O(1), but O(log(n)) under relaxed requirements
+(see above).
+
+=head2 mark_items_as_done
+
+Given any number of  C<Queue::Q::ClaimFIFO::Item> objects that were
+previously claimed from this queue, they are removed from the claimed-items
+tracking and thus removed from the queue altogether.
+
+Complexity: Generally O(n), but O(n*log(n)) under relaxed requirements
+(see above) with n understood to be the number of items to mark as done.
 
 =head2 queue_length
 
