ab22d7892970963990f52eefb1e1ba565b7cd5a2
Steffen Mueller <smueller@cpan.org>
2012-10-19 07:46:42 +0200

    Docs for NaiveFIFO::Perl

--- lib/Queue/Q/NaiveFIFO/Perl.pm
+++ lib/Queue/Q/NaiveFIFO/Perl.pm
@@ -49,3 +49,44 @@ sub queue_length {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Queue::Q::NaiveFIFO::Perl - In-memory Perl implementation of the NaiveFIFO queue
+
+=head1 SYNOPSIS
+
+  use Queue::Q::NaiveFIFO::Perl;
+  my $q = Queue::Q::NaiveFIFO::Perl->new;
+  $q->enqueue_item("foo");
+  my $foo = $q->claim_item;
+
+=head1 DESCRIPTION
+
+Implements interface defined in L<Queue::Q::NaiveFIFO>:
+a very simple in-memory implementation using a Perl array
+as the queue.
+
+=head1 METHODS
+
+All methods of L<Queue::Q::NaiveFIFO> plus:
+
+=head2 new
+
+Constructor. Takes no parameters.
+
+=head1 AUTHOR
+
+Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Steffen Mueller
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
