f2c95932cbe42e2d2bdad3df7025439fbd1b347a
Herald van der Breggen <herald@vdbreggen.com>
2012-12-10 20:02:58 +0100

    added fail_count and last_error. Also documentation and cleanup

--- lib/Queue/Q/ReliableFIFO/Item.pm
+++ lib/Queue/Q/ReliableFIFO/Item.pm
@@ -11,11 +11,20 @@ my $deserializer = JSON::XS->new->pretty(0);
 #use Sereal::Decoder;
 #my $serializer   = Sereal::Encoder->new();
 #my $deserializer = Sereal::Decoder->new();
-use Data::Dumper;
+
+my @item_info = (
+    't',        # time the item was created
+    'rc',       # requeue counter (how often the item is requeued)
+    'fc',       # fail counter (how often the item reached max-requeue)
+    'error',    # last error message
+);
 
 sub new {
     my $class = shift;
     my $self = bless { @_ }, $class;
+
+    die "data or _serialized expected for new\n"
+        if (!exists $self->{data} && !exists $self->{_serialized});
 
     if (!exists $self->{_serialized}) {
         $self->{_serialized} ||=
@@ -26,9 +35,11 @@ sub new {
     }
     return $self;
 }
-sub time_created  { shift->_get('time_created'); }
+sub time_created  { shift->_get('t'); }
 sub data          { shift->_get('data'); }
-sub requeue_count { shift->_get('requeue_count') || 0; }
+sub requeue_count { shift->_get('rc') || 0; }
+sub fail_count    { shift->_get('fc') || 0; }
+sub last_error    { shift->_get('error'); }
 sub _get { 
     my ($self, $elem) = @_;
     $self->_deserialize if ! exists $self->{data};
@@ -38,15 +49,74 @@ sub _serialized { $_[0]->{_serialized}; }
 sub inc_nr_requeues {
     my $self = shift;
     my $plain = $deserializer->decode($self->{_serialized});
-    $self->{requeue_count} = ++$plain->{rc};
+    $self->{rc} = ++$plain->{rc};
     $self->{_serialized} =  $serializer->encode($plain);
-    return $self->{requeue_count};
+    return $self->{rc};
 }
 sub _deserialize {
     my $self = shift;
     my $plain = $deserializer->decode($self->{_serialized});
-    $self->{time_created} = $plain->{t};
-    $self->{requeue_count} = $plain->{rc} if exists $plain->{rc};
     $self->{data} = $plain->{b};
+    for my $k (@item_info) {
+        if (exists $plain->{$k}) {
+            $self->{$k} = $plain->{$k};
+        }
+        else {
+            delete $self->{$k} if exists $self->{$k};
+        }
+    }
 }
 1;
+__END__
+
+=head1 NAME
+
+Queue::Q::ReliableFIFO::Item - An item object of an queue item
+
+=head1 SYNOPSIS
+
+  use Queue::Q::ReliableFIFO::Item;
+  my $item = Queue::Q::ReliableFIFO::Item->new(data => { id => 23 });
+  my $time_created = $item->time_created;   # epoch time
+
+
+  # items can be created from serialized data (to be used by 
+  # Queue::Q::ReliableFIFO::Redis).
+  my $item = Queue::Q::ReliableFIFO::Item->new(_serialized => $serialized);
+  my $data = $item->data();
+  my $last_error = $item->last_error();
+
+=head1 METHODS
+
+=head2 $item = new(data => $data);
+
+Constructor for an item object (key "data" for plain data that needs to 
+be serialized, key _serialized for data that needs to be deserialzed).
+
+=head2 $data = $item->data();
+
+Returns the data where the item was created from.
+
+=head2 $time_created = $item->time_created();
+
+Returns de time (epoch) the item was originally created (put in the queue).
+
+=head2 $n = $item->requeue_count();
+
+Returns the number the item was requeued. Requeuing can happen when
+processing the item fails.
+
+=head2 $n = $item->fail_count();
+
+Returns the number the item was requeued. An item fails after the it has
+been retried up to requeue_limit times (See Redis.pm).
+
+=head2 $n = $item->last_error();
+
+Returns the last error message of processing this item.
+
+=head2 inc_nr_requeues();
+
+Increases the requeue counter of this item.
+
+=cut
